d62f3337794c8ce5eebbf5eef4776002d24812f8
wertylu <alfeziat@gmail.com>
2024-04-10 21:15:27 +0300

    added flashlight

--- lib/carstat/app/home_page.dart
+++ lib/carstat/app/home_page.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
+import 'dart:io';
+
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flash_light_control_plugin/flash_light_control_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:my_project/carstat/app/add_car_form.dart';
 import 'package:my_project/carstat/home/bottom_part.dart';
@@ -31,6 +34,29 @@ class _MainPageState extends State<MainPage> {
         _showNoInternetDialog();
       }
     });
+    if (Platform.isIOS) {
+      _showIOSNotification();
+    }
+  }
+
+  void _showIOSNotification() {
+    if (Platform.isIOS) {
+      showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Notice'),
+            content: const Text('YOU IOS, NO FLASHLIGHT'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   void _showNoInternetDialog() {
@@ -71,42 +97,63 @@ class _MainPageState extends State<MainPage> {
         backgroundColor: Colors.deepPurple,
       ),
       drawer: const CustomDrawer(),
-      body: FutureBuilder<List<Car>>(
-        future: _carListFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (snapshot.hasData) {
-            final cars = snapshot.data!;
-            return ListView.builder(
-              itemCount: cars.length,
-              itemBuilder: (context, index) {
-                final car = cars[index];
-                return ListTile(
-                  title: Text('${car.make} ${car.model}'),
-                  subtitle: Text(
-                      'Year: ${car.year} - Mileage: ${car.mileage} - 0 to 60: ${car.zero_to_sixty}s'),
-                  trailing: Wrap(
-                    spacing: 12,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.delete, color: Colors.red),
-                        onPressed: () async {
-                          await _carService.deleteCar(car.id); // Assuming `deleteCar` method uses the car's id
-                          refreshCars(); // Call `refreshCars` to update the list after deletion
-                        },
-                      ),
-                    ],
-                  ),
-                );
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder<List<Car>>(
+              future: _carListFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (snapshot.hasData) {
+                  final cars = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: cars.length,
+                    itemBuilder: (context, index) {
+                      final car = cars[index];
+                      return ListTile(
+                        title: Text('${car.make} ${car.model}'),
+                        subtitle: Text(
+                            'Year: ${car.year} - Mileage: ${car.mileage} - '
+                                '0 to 60: ${car.zero_to_sixty}s',),
+                        trailing: Wrap(
+                          spacing: 12,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.delete, color: Colors.red),
+                              onPressed: () async {
+                                await _carService.deleteCar(car.id);
+                                refreshCars();
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  return const Center(child: Text('No cars added yet.'));
+                }
               },
-            );
-          } else {
-            return const Center(child: Text('No cars added yet.'));
-          }
-        },
+            ),
+          ),
+          // Adding the Row with ElevatedButtons here
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: FlashLightControlPlugin.turnOn,
+                child: Text('Flashlight On'),
+              ),
+              ElevatedButton(
+                onPressed: FlashLightControlPlugin.turnOff,
+                child: Text('Flashlight Off'),
+              ),
+            ],
+          ),
+        ],
       ),
       bottomNavigationBar: CustomBottomAppBar(
         onAddPressed: () async {
